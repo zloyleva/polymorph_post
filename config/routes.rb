3987821515d5ac4945d1ab5afbc9c95f7d30504b
zloyleva <zloyleva@gmail.com>
2016-11-03 22:34:15 +0200

    Add create form for comments for comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :comments do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
