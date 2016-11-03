bc2bd598ecd2739fb4771f5249a46f15f85c936f
zloyleva <zloyleva@gmail.com>
2016-11-03 15:10:13 +0200

    Create polymorphic has_many relationships and migration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  has_many :comments, as: :commentable
+  belongs_to :commentable, polymorphic: true
 end
--- db/migrate/20161103125042_create_comments.rb
+++ db/migrate/20161103125042_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :body
+      t.references :commentable, polymorphic: true
 
       t.timestamps
     end
