2aca4b46d1becdf2f982a394630313b94a0529b3
zloyleva <zloyleva@gmail.com>
2016-11-03 22:41:33 +0200

    Add a selection parent

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    @parent = Post.find_by_id(params[:post_id])
+    @parent = Post.find_by_id(params[:post_id]) if params[:post_id]
+    @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
 
     @comment  =  @parent.comments.new( body:  comment_params[:body])
     @comment.save
