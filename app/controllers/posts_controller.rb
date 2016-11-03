a86d9df2072f14c90c00b3e4f933bd38896571c2
zloyleva <zloyleva@gmail.com>
2016-11-03 17:34:55 +0200

    Add create action to Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to root_url
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
