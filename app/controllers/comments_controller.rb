4ddfe0725705306d7fd211bb53f3f5e60e7a686c
zloyleva <zloyleva@gmail.com>
2016-11-03 20:36:52 +0200

    Add action create for Comment-Post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @parent = Post.find_by_id(params[:post_id])
+
+    @comment  =  @parent.comments.new( body:  comment_params[:body])
+    @comment.save
+
+    redirect_to root_url
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
